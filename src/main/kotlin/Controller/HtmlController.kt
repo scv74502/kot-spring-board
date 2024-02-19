620ff29b288390f2d32beb07915b0d7efc5c9234
김채윤 <kimchaeyun@gimchaeyun-ui-MacBookAir.local>
2024-02-19 14:46:18 +0900

    init

---
+++ src/main/kotlin/Controller/HtmlController.kt
@@ -0,0 +1,15 @@
+package Controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HtmlController {
+    @GetMapping("/")
+    fun blog(): String {
+        return "blog"
+    }
+}
---
+++ src/test/kotlin/org/example/assignment/Test/IntegrationTests.kt
@@ -0,0 +1,17 @@
+//package org.example.assignment.Test
+//
+//import org.junit.jupiter.api.BeforeAll
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.boot.test.web.client.TestRestTemplate
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+//
+//    @BeforeAll
+//    fun setup(){
+//        println(">> setup")
+//    }
+//
+//    @
+//}
