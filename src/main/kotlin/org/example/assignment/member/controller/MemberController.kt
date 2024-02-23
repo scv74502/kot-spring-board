5954fe0cdcbb7521fa8de1911e626822952ff390
kunwoo park <scv74502@gmail.com>
2024-02-23 16:08:53 +0900

    member with auth controller done, need to tested

--- src/main/kotlin/org/example/assignment/member/controller/MemberController.kt
+++ src/main/kotlin/org/example/assignment/member/controller/MemberController.kt
@@ -3,6 +3,7 @@ package org.example.assignment.member.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.example.assignment.common.dto.ApiResponse
+import org.example.assignment.member.dto.MemberUpdateRequest
 import org.example.assignment.member.service.MemberService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -21,4 +22,15 @@ class MemberController (
     @Operation(summary = "회원의 자기 자신 정보 조회하기")
     @GetMapping
     fun getMemberInfo(@AuthenticationPrincipal user: User) =
-        ApiResponse.success(memberService.getMemberInfoByLoginId
+        ApiResponse.success(memberService.getMemberInfo(user.username))
+
+    @Operation(summary = "회원의 삭제하기")
+    @DeleteMapping
+    fun deleteMember(@AuthenticationPrincipal user: User) =
+        ApiResponse.success(memberService.deleteMember(user.username.toLong()))
+
+    @Operation(summary = "회원의 자기 정보 수정하기")
+    @PutMapping
+    fun updateMember(@AuthenticationPrincipal user: User, @RequestBody request: MemberUpdateRequest) =
+        ApiResponse.success(memberService.updateMember(user.username.toLong(), request))
+}
