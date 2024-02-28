984c28eb9fbe8fc26b2a48e86cc374156a96fdfc
kunwoo park <scv74502@gmail.com>
2024-02-28 15:51:13 +0900

    querydsl config done

--- src/main/kotlin/org/example/assignment/comment/repository/CommentRepository.kt
+++ src/main/kotlin/org/example/assignment/comment/repository/CommentRepository.kt
@@ -1,13 +1,8 @@
 package org.example.assignment.comment.repository
 
-import org.example.assignment.comment.dto.CommentResponseDto
 import org.example.assignment.comment.entity.Comment
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.function.Consumer
 
 
 interface CommentRepository: JpaRepository<Comment, Long> {
-//    fun findByArticleId(id: Long): List<CommentResponseDto> {
-//
-//    }
 }
